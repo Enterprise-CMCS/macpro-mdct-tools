a57a5a2495bd2f6231e6817be3bf03b1af01ef47
Berry Davenport <berry.davenport@stratiform.digital>
2024-05-22 16:26:42 -0400

    Fix java path being written repeatedly to shell profile

--- mdct-setup.sh
+++ mdct-setup.sh
@@ -129,18 +129,17 @@ fi
 # Install java with brew 
 if ! which java > /dev/null ; then
 	brew install java
-fi
-
-# Check if java is installed and add it to PATH if necessary
-if ! which java > /dev/null ; then
-    echo "Java installation failed."
-else
-    # Get the directory where java is installed
-    java_home=$(brew --prefix)/opt/openjdk@11
-    
-    # Add java to PATH
-    echo "export PATH=\"$java_home/bin:\$PATH\"" >> "$shellprofile"
-    # echo "Java installed successfully and added to PATH."
+  # Check if java is installed and add it to PATH if necessary
+  if ! which java > /dev/null ; then
+      echo "Java installation failed." && exit 1
+  else
+      # Get the directory where java is installed
+      java_home=$(brew --prefix)/opt/openjdk@11
+      
+      # Add java to PATH
+      echo "export PATH=\"$java_home/bin:\$PATH\"" >> "$shellprofile"
+      # echo "Java installed successfully and added to PATH."
+  fi  
 fi
 
 # Install awslogs, a utility for streaming CloudWatch logs
