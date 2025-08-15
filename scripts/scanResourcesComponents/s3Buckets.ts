f7cca792c7b087c74d189285aaa67c991c1f07a8
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-08-15 11:31:33 -0400

    Update s3Buckets.ts

--- scripts/scanResourcesComponents/s3Buckets.ts
+++ scripts/scanResourcesComponents/s3Buckets.ts
@@ -3,13 +3,9 @@ import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
 
 const client = new S3Client({ region: "us-east-1" });
 
-const excludedPrefixes = ["cms-cloud"];
-
 export async function getAllS3Buckets(): Promise<string[]> {
   const r = await client.send(new ListBucketsCommand({}));
-  return r
-    .Buckets!.map((b) => b.Name!)
-    .filter((s) => !excludedPrefixes.some((prefix) => s.startsWith(prefix)));
+  return r.Buckets!.map((b) => b.Name!);
 }
 
 async function main() {
